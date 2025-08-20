2bd200b027253a7fb791bf4c5eb0312a4185fb05
R0dner <ronisp200@gmail.com>
2025-08-20 15:51:52 -0400

    añadiendo debusgs en offer

--- components/landing-one/Offer.vue
+++ components/landing-one/Offer.vue
@@ -1,20 +1,27 @@
 <template lang="pug">
 section.offer-area.ptb-60
   .container
-    .row
-      // Añadir v-if para verificar que los datos existen
-      .col-lg-4.col-md-6(v-for="category in categoria.data" :key="category.id" v-if="categoria && categoria.data")
+    .row(v-if="categoria && categoria.data && categoria.data.length")
+      .col-lg-4.col-md-6(v-for="category in categoria.data" :key="category.id")
         .offer-box
-          img(:src="api_url + category.attributes.imagen.data.attributes.url" height="400")
+          img(
+            v-if="category.attributes.imagen && category.attributes.imagen.data"
+            :src="api_url + category.attributes.imagen.data.attributes.url" 
+            height="400"
+            :alt="category.attributes.nombre"
+          )
           .category
             h4 {{ category.attributes.nombre }}
           .box-inner
             .inner-content
               h3 {{ category.attributes.descripcion }}
-              ul
+              ul(v-if="category.attributes.grupos_de_productos && category.attributes.grupos_de_productos.data")
                 li(v-for="grupo in category.attributes.grupos_de_productos.data" :key="grupo.id")
                   router-link(:to="{ name: 'products', query: { grupo_producto: grupo.id } }")
                     | {{ grupo.attributes.nombre }}
+    .row(v-else)
+      .col-12
+        p.text-center Cargando categorías...
 </template>
 
 <script>
