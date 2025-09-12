6724ce1c610de327b89ab16af6b959ac4b8ccbf0
R0dner <ronisp200@gmail.com>
2025-09-12 16:43:34 -0400

    orden en banner

--- components/landing-one/Banner.vue
+++ components/landing-one/Banner.vue
@@ -7,8 +7,8 @@
                     <div class="main-banner-content white-color">
                         <span>Nueva colección 2024</span>
                         <h1>Estilo que define quién eres</h1>
-                        <p>Descubre las últimas tendencias para todos los estilos y personalidades</p>
-                        
+                        <p>Descubre las últimas tendencias para todos los </p>
+                        <p> estilos y personalidades</p>
                         <nuxt-link to="/blog-one" class="btn btn-primary">PRODUCTOS DESTACADOS</nuxt-link>
                     </div>
                 </div>
